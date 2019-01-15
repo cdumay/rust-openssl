1290e24e29adf8475cfe4ea8ae803855ed3f9e9d
Cédric Dumay <cedric.dumay@gmail.com>
2019-01-15 11:51:08 +0100

    devel: enable TLSV1_3 for mozilla

--- openssl/src/ssl/connector.rs
+++ openssl/src/ssl/connector.rs
@@ -197,8 +197,8 @@ impl SslAcceptor {
     /// [docs]: https://wiki.mozilla.org/Security/Server_Side_TLS
     pub fn mozilla_intermediate(method: SslMethod) -> Result<SslAcceptorBuilder, ErrorStack> {
         let mut ctx = ctx(method)?;
-        #[cfg(ossl111)]
-        ctx.set_options(SslOptions::NO_TLSV1_3);
+//        #[cfg(ossl111)]
+//        ctx.set_options(SslOptions::NO_TLSV1_3);
         let dh = Dh::params_from_pem(
             b"
 -----BEGIN DH PARAMETERS-----
@@ -235,8 +235,8 @@ ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
     pub fn mozilla_modern(method: SslMethod) -> Result<SslAcceptorBuilder, ErrorStack> {
         let mut ctx = ctx(method)?;
         ctx.set_options(SslOptions::NO_TLSV1 | SslOptions::NO_TLSV1_1);
-        #[cfg(ossl111)]
-        ctx.set_options(SslOptions::NO_TLSV1_3);
+//        #[cfg(ossl111)]
+//        ctx.set_options(SslOptions::NO_TLSV1_3);
         setup_curves(&mut ctx)?;
         ctx.set_cipher_list(
             "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:\
